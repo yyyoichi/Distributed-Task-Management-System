d66f550eea2487e2524b421fd20365a701ea9864
yyyoichi <origamiichiro@gmail.com>
2023-10-29 10:51:40 +0000

    feat add key-value store

---
+++ database/database_test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"sync"
+	"testing"
+)
+
+func TestSaveTODO(t *testing.T) {
+	test := []struct {
+		tasks       []string
+		expectedLen int
+	}{
+		{[]string{"TODO: A", "TODO: B", "TODO: C"}, 3},
+		{[]string{"TODO: D", "TODO: E"}, 2},
+	}
+	for i, tt := range test {
+		store := NewStore()
+		var wg sync.WaitGroup
+		wg.Add(len(tt.tasks))
+		for _, task := range tt.tasks {
+			go func(t string) {
+				defer wg.Done()
+				store.Save(t)
+			}(task)
+		}
+		wg.Wait()
+		if len(store.ByID) != tt.expectedLen {
+			t.Errorf("%d:Expected length is %d, but got='%d'", i, tt.expectedLen, len(store.ByID))
+		}
+	}
+}
--- database/main.go
+++ database/main.go
@@ -1,7 +1,70 @@
 package main
 
-import "log"
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"sync"
+)
+
+var (
+	ErrInvalidBodyProperty = errors.New("invalid body property")
+)
 
 func main() {
-	log.Panicln("start database")
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Println("port is not found")
+	}
+	addr := fmt.Sprintf(":%s", port)
+
+	var store = NewStore()
+
+	http.HandleFunc("/", store.handler)
+	if err := http.ListenAndServe(addr, nil); err != nil {
+		log.Println(err)
+	}
+	log.Println("start database")
+}
+
+func NewStore() TStore { return TStore{ByID: make(map[int]string)} }
+
+type TStore struct {
+	mu   sync.Mutex
+	ByID map[int]string
+}
+
+type ReqJson struct {
+	Task string `validate:"required"`
+}
+
+func (s *TStore) handler(w http.ResponseWriter, r *http.Request) {
+	var data *ReqJson
+	if err := json.NewDecoder(r.Body).Decode(data); err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	s.Save(data.Task)
+	w.WriteHeader(http.StatusOK)
+}
+
+func (s *TStore) Save(task string) {
+	s.mu.Lock()
+	id := s.nextID()
+	s.ByID[id] = task
+	s.mu.Unlock()
+}
+
+func (s *TStore) nextID() int {
+	max := 0
+	for id := range s.ByID {
+		if max < id {
+			max = id
+		}
+	}
+	return max + 1
 }
