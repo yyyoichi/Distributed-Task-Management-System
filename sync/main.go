f8fd1bd5ce8e414062e007e97d74b4598667c344
yyyoichi <origamiichiro@gmail.com>
2023-11-01 12:16:19 +0000

    feat use env

--- sync/main.go
+++ sync/main.go
@@ -1,7 +1,50 @@
 package main
 
-import "log"
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+var (
+	ENV_STORES     = os.Getenv("STORES")
+	ENV_POLLING_MS = os.Getenv("POLLING_MS")
+)
 
 func main() {
+	if ENV_STORES == "" {
+		log.Println("export 'ENV_STORE' is not found")
+		return
+	}
+	if ENV_POLLING_MS == "" {
+		log.Println("export 'ENV_POLLING_MS' is not found")
+		return
+	}
+
+	urls := []string{}
+	for _, schema := range strings.Split(ENV_STORES, ",") {
+		urls = append(urls, fmt.Sprintf("http://%s", schema))
+	}
+	pollingMillSecond, err := strconv.Atoi(ENV_POLLING_MS)
+	if err != nil {
+		log.Println("'ENV_POLLING_MS' is not int")
+	}
+
+	cxt := context.WithoutCancel(context.Background())
+
 	log.Println("start sync")
+	go func() {
+		for {
+			select {
+			case <-cxt.Done():
+				return
+			case <-time.After(time.Duration(pollingMillSecond) * time.Millisecond):
+				// polling
+			}
+		}
+	}()
 }
