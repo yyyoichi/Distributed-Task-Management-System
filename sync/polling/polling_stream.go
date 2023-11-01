cf7ca17b10e04a067a16bf367d287b1da8bcc885
yyyoichi <origamiichiro@gmail.com>
2023-11-01 12:07:46 +0000

    feat streams to use channel safely

--- sync/polling/polling_stream.go
+++ sync/polling/polling_stream.go
@@ -6,10 +6,46 @@ import (
 	"yyyoichi/Distributed-Task-Management-System/sync/api"
 )
 
-func generateURL(cxt context.Context, urls []string) <-chan string {
-	return stream.Generator[string](cxt, urls...)
+// 差分探知機
+type DifferenceDetector struct {
+	SyncerID int
+	Get      func() api.DiffResponse
 }
 
-func getDifferenceUseFunIO(cxt context.Context, urlCh <-chan string, fn func(url string) api.DiffResponse) <-chan api.DiffResponse {
-	return stream.FunIO[string, api.DiffResponse](cxt, urlCh, fn)
+// 差分探知機をジェネレータとして送信する
+func GenerateDifferenceDetector(cxt context.Context, detectors []DifferenceDetector) <-chan DifferenceDetector {
+	return stream.Generator[DifferenceDetector](cxt, detectors...)
+}
+
+// 差分情報
+type Differences struct {
+	SyncerID     int
+	DiffResponse api.DiffResponse
+}
+
+// 差分探知機から差分情報をパイプする
+func LineDetector2Differences(cxt context.Context, detectorCh <-chan DifferenceDetector, fn func(DifferenceDetector) Differences) <-chan Differences {
+	return stream.FunIO[DifferenceDetector, Differences](cxt, detectorCh, fn)
+}
+
+// 同期実行機
+type Synchronizer struct {
+	SyncerID int
+	Exec     func() api.SyncResponse
+}
+
+// 同期実行機をジェネレータとして送信する
+func GenerateSyncronizer(cxt context.Context, synchronizers []Synchronizer) <-chan Synchronizer {
+	return stream.Generator[Synchronizer](cxt, synchronizers...)
+}
+
+// 同期結果情報
+type Dones struct {
+	SyncerID     int
+	SyncResponse api.SyncResponse
+}
+
+// 同期実行機から同期結果情報をパイプする
+func LineSynchronizer2Dones(cxt context.Context, synchronizerCh <-chan Synchronizer, fn func(Synchronizer) Dones) <-chan Dones {
+	return stream.FunIO[Synchronizer, Dones](cxt, synchronizerCh, fn)
 }
