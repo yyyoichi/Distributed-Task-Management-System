16e209f6f096288c58769cd9c82d2ddfb84b5e31
yyyoichi <origamiichiro@gmail.com>
2023-11-02 09:28:33 +0000

    feat call polling

--- sync/main.go
+++ sync/main.go
@@ -8,6 +8,7 @@ import (
 	"strconv"
 	"strings"
 	"time"
+	"yyyoichi/Distributed-Task-Management-System/sync/polling"
 )
 
 var (
@@ -16,6 +17,7 @@ var (
 )
 
 func main() {
+	// 環境変数バリデーション
 	if ENV_STORES == "" {
 		log.Println("export 'ENV_STORE' is not found")
 		return
@@ -25,16 +27,20 @@ func main() {
 		return
 	}
 
+	// データノードのリクエストURK
 	urls := []string{}
 	for _, schema := range strings.Split(ENV_STORES, ",") {
 		urls = append(urls, fmt.Sprintf("http://%s", schema))
 	}
+	// 同期機構のポーリング間隔
 	pollingMillSecond, err := strconv.Atoi(ENV_POLLING_MS)
 	if err != nil {
 		log.Println("'ENV_POLLING_MS' is not int")
 	}
 
+	// 処理開始
 	cxt := context.WithoutCancel(context.Background())
+	pm := polling.NewPollingManager(urls)
 
 	log.Println("start sync")
 	go func() {
@@ -44,7 +50,9 @@ func main() {
 				return
 			case <-time.After(time.Duration(pollingMillSecond) * time.Millisecond):
 				// polling
+				pm.Polling(cxt)
 			}
 		}
 	}()
+	select {}
 }
